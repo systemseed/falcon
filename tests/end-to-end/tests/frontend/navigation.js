8e16e73a8b141e5f59e815b1aaaff399abcc34c7
pavelbulat <bulat.pasha@gmail.com>
2019-08-13 11:03:45 +0300

    fix(frontend): Test CI. [#166899524]

--- tests/end-to-end/tests/frontend/navigation.js
+++ tests/end-to-end/tests/frontend/navigation.js
@@ -1,8 +1,11 @@
 import { Selector, ClientFunction } from 'testcafe';
 import * as config from '../utils/config';
+import Page from '../utils/frontend-model';
 
 fixture('Navigation')
   .page(config.frontendURL).httpAuth(config.httpAuth).beforeEach(config.beforeEach);
+
+const page = new Page();
 
 test
   ('Top menu links work without errors', async t => {
@@ -24,6 +27,7 @@ test
 
     for (let i = 0; i < count; i++) {
       await t.click(menuItems.nth(i));
+      await page.pageLoaded();
       const getPathname = ClientFunction(() => window.location.pathname);
       await t.expect(await getPathname()).eql(menuUrls[i], `Current pathname = ${menuUrls[i]}`, { timeout: 5000 });
       await t.expect(Selector('.page-content .error-page').exists).notOk('No 40x/50x error on the page.');
