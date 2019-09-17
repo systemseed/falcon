e6b69149369f39d7120a21e953f75c165effe906
pavelbulat <bulat.pasha@gmail.com>
2019-09-17 14:27:48 +0300

    ci: Changed post-deploy.sh. [#167535416]

--- .deployment/scripts/post-deploy.sh
+++ .deployment/scripts/post-deploy.sh
@@ -20,7 +20,7 @@ POD_ID=$(kubectl -n $NAMESPACE get pods -l "release=falcon-$BRANCH,component=$CO
 echo "Selected POD ID for executing post-deployment tasks: $POD_ID"
 
 echo "Install drupal"
-time kubectl -n $NAMESPACE exec -it $POD_ID -c php -- bash -c 'drush -r web site-install falcon	--db-url=mysql://$(DB_USER):$(DB_PASSWORD)@$(DB_HOST)/$(DB_NAME) --site-name=$(PROJECT_NAME) --account-pass=admin install_configure_form.enable_update_status_module=NULL --yes; (exit $?)'
+time kubectl -n $NAMESPACE exec -it $POD_ID -c php -- bash -c 'drush -r web site-install falcon	--site-name=$(PROJECT_NAME) --account-pass=admin install_configure_form.enable_update_status_module=NULL --yes; (exit $?)'
 echo "Enabling development modules"
 time kubectl -n $NAMESPACE exec -it $POD_ID -c php -- bash -c 'drush -r web en $(DEVELOPMENT_MODULES); (exit $?)'
 echo "Generate sitemap"
